297f4ec73193af99be049bbc277f949982a450c2
admin <admin@orillusion.com>
2021-10-30 16:55:24 +0800

    add local bikeshed build.sh

--- webgpu/build.sh
+++ webgpu/build.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/bash
 
 echo "Start building"
-#build en
-curl https://api.csswg.org/bikeshed/ -F file=@webgpu/spec/index.bs -F force=1 > docs/.vuepress/dist/webgpu.html
-curl https://api.csswg.org/bikeshed/ -F file=@webgpu/wgsl/index.bs -F force=1 > docs/.vuepress/dist/wgsl.html
-# build zh
-curl https://api.csswg.org/bikeshed/ -F file=@webgpu/zh/spec/index.bs -F force=1 > docs/.vuepress/dist/zh/webgpu.html
-curl https://api.csswg.org/bikeshed/ -F file=@webgpu/zh/wgsl/index.bs -F force=1 > docs/.vuepress/dist/zh/wgsl.html
+
+echo "build en spec"
+bikeshed --die-on=everything spec webgpu/spec/index.bs
+cp webgpu/spec/index.html docs/.vuepress/dist/webgpu.html
+
+echo "build en wgsl"
+bikeshed --die-on=everything spec webgpu/wgsl/index.bs
+cp webgpu/wgsl/index.html docs/.vuepress/dist/wgsl.html
+
+echo "build zh spec"
+bikeshed --die-on=everything spec webgpu/zh/spec/index.bs
+cp webgpu/zh/spec/index.html docs/.vuepress/dist/zh/webgpu.html
+
+echo "build zh wgsl"
+bikeshed --die-on=everything spec webgpu/zh/wgsl/index.bs
+cp webgpu/zh/wgsl/index.html docs/.vuepress/dist/zh/wgsl.html
+
 echo "Finished"
