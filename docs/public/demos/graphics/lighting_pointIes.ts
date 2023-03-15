53cd993f8f44f932c318234ff6fa19aa688852cb
ShuangLiu <liu.shuang@huasheng.io>
2023-03-15 19:51:58 +0800

    fix ies demo

--- docs/public/demos/graphics/lighting_pointIes.ts
+++ docs/public/demos/graphics/lighting_pointIes.ts
@@ -22,7 +22,7 @@ export class Sample_LightIES {
     this.scene.addChild(cameraObj);
 
     //set camera data
-    this.hoverCameraController.setCamera(0, -45, 1200);
+    this.hoverCameraController.setCamera(0, -45, 200);
     await this.initScene(this.scene);
 
     let renderJob = new ForwardRenderJob(this.scene);
@@ -43,9 +43,9 @@ export class Sample_LightIES {
         pl.castShadow = true;
         pl.realTimeShadow = true;
         pl.iesPofile = iesPofiles;
-        po.x = 26;
+        po.x = 0;
         po.y = 22;
-        po.z = -174.91 ;
+        po.z = 15 ;
         this.scene.addChild(po);
         pl.debug();
         pl.debugDraw(true);
@@ -62,49 +62,21 @@ export class Sample_LightIES {
         mat.roughness = 0.5;
         mat.metallic = 0.2;
 
-        let floor = new Object3D();
-        let floorMr = floor.addComponent(MeshRenderer);
-        floorMr.geometry = new BoxGeometry(50000, 5, 50000);
-        floorMr.material = mat;
-        this.scene.addChild(floor);
-
         ball = new Object3D();
         let mr = ball.addComponent(MeshRenderer);
         mr.geometry = new SphereGeometry(6, 20, 20);
         mr.material = mat;
         this.scene.addChild(ball);
         ball.transform.x = -17;
-        ball.transform.y = 34;
-        ball.transform.z = 23;
+        ball.transform.y = 10;
+        ball.transform.z = 10;
 
         //wall
         let back_wall =  new Object3D();
         let mr2 = back_wall.addComponent(MeshRenderer);
         mr2.geometry = new BoxGeometry(500,500,10);
         mr2.material = mat;
-        back_wall.z = - 200 ;
-        this.scene.addChild(back_wall);
-
-        let front_wall =  new Object3D();
-        let mr3 = front_wall.addComponent(MeshRenderer);
-        mr3.geometry = new BoxGeometry(500,500,10);
-        mr3.material = mat;
-        front_wall.z = 200 ;
-        this.scene.addChild(front_wall);
-
-        let left_wall =  new Object3D();
-        let mr4 = left_wall.addComponent(MeshRenderer);
-        mr4.geometry = new BoxGeometry(10,500,500);
-        mr4.material = mat;
-        left_wall.x = - 200 ;
-        this.scene.addChild(left_wall);
-
-        let right_wall =  new Object3D();
-        let mr5 = right_wall.addComponent(MeshRenderer);
-        mr5.geometry = new BoxGeometry(10,500,500);
-        mr5.material = mat;
-        right_wall.x = 200 ;
-        this.scene.addChild(right_wall);
+        this.scene.addChild(back_wall);       
     }
   }
 }
