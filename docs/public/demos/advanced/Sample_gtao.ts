402cd667f2b53dc0bdbeab0515b93f8dc56e3661
ShuangLiu <liu.shuang@huasheng.io>
2023-05-30 13:37:41 +0800

    update sample

--- docs/public/demos/advanced/Sample_gtao.ts
+++ docs/public/demos/advanced/Sample_gtao.ts
@@ -1,111 +1,124 @@
-import { DirectLight, Engine3D, PostProcessingComponent, View3D, LitMaterial, HoverCameraController, KelvinUtil, MeshRenderer, Object3D, PlaneGeometry, Scene3D, SphereGeometry, Time, CameraUtil, webGPUContext, AtmosphericComponent, GTAOPost, Vector3, TAAPost, SSRPost } from '@orillusion/core'
-import * as dat from 'dat.gui'
+import {
+	View3D, DirectLight, Engine3D,
+	PostProcessingComponent, LitMaterial, HoverCameraController,
+	KelvinUtil, MeshRenderer, Object3D, PlaneGeometry, Scene3D, SphereGeometry,
+	CameraUtil, webGPUContext, BoxGeometry, TAAPost, AtmosphericComponent, GTAOPost
+} from '@orillusion/core';
+import * as dat from 'dat.gui';
 
-export class Sample_GTAO {
-    lightObj: Object3D
-    scene: Scene3D
-    constructor() {}
+class Sample_GTAO {
+	lightObj: Object3D;
+	scene: Scene3D;
 
-    async run() {
-        Engine3D.setting.shadow.enable = true
-        Engine3D.setting.shadow.shadowBound = 40
-        Engine3D.setting.shadow.shadowBias = 0.0001
-        Engine3D.setting.shadow.autoUpdate = true
-        Engine3D.setting.shadow.updateFrameRate = 1
-        await Engine3D.init({
-            canvasConfig: {
-                devicePixelRatio: 1
-            },
-            renderLoop: () => this.loop()
-        })
+	async run() {
+		Engine3D.setting.shadow.shadowSize = 2048
+		Engine3D.setting.shadow.shadowBound = 500;
+		Engine3D.setting.shadow.shadowBias = 0.0002;
 
-        this.scene = new Scene3D()
-        this.scene.addComponent(AtmosphericComponent).sunY = 0.6
+		await Engine3D.init();
 
-        let mainCamera = CameraUtil.createCamera3DObject(this.scene, 'camera')
-        mainCamera.perspective(60, webGPUContext.aspect, 1, 5000.0)
-        let ctrl = mainCamera.object3D.addComponent(HoverCameraController)
-        ctrl.setCamera(100, -15, 40)
-        await this.initScene(this.scene)
+		this.scene = new Scene3D();
+		this.scene.addComponent(AtmosphericComponent).sunY = 0.6;
 
-        let view = new View3D()
-        view.scene = this.scene
-        view.camera = mainCamera
-        Engine3D.startRenderView(view)
+		let mainCamera = CameraUtil.createCamera3DObject(this.scene, 'camera');
+		mainCamera.perspective(60, webGPUContext.aspect, 1, 5000.0);
+		let ctrl = mainCamera.object3D.addComponent(HoverCameraController);
+		ctrl.setCamera(0, -15, 500);
+		await this.initScene();
 
-        let postProcessing = this.scene.addComponent(PostProcessingComponent)
-        postProcessing.addPost(GTAOPost)
+		let view = new View3D();
+		view.scene = this.scene;
+		view.camera = mainCamera;
+		Engine3D.startRenderView(view);
 
-        // load a test gltf model
-        let data = await Engine3D.res.loadGltf('https://cdn.orillusion.com/PBR/Duck/Duck.gltf')
-        this.scene.addChild(data)
-        data.scaleX = data.scaleY = data.scaleZ = 0.05
-    }
+		let postProcessing = this.scene.addComponent(PostProcessingComponent);
+		let post = postProcessing.addPost(GTAOPost);
+		post.maxDistance = 60;
+		this.gui();
+	}
 
-    async initScene(scene: Scene3D) {
-        /******** light *******/
-        {
-            this.lightObj = new Object3D()
-            this.lightObj.rotationX = 15
-            this.lightObj.rotationY = 110
-            this.lightObj.rotationZ = 0
-            let lc = this.lightObj.addComponent(DirectLight)
-            lc.lightColor = KelvinUtil.color_temperature_to_rgb(5355)
-            lc.castShadow = true
-            lc.intensity = 10
-            lc.needUpdateShadow = true
-            scene.addChild(this.lightObj)
-        }
-        this.createPlane(scene)
-        return true
-    }
+	async initScene() {
+		{
+			this.lightObj = new Object3D();
+			this.lightObj.rotationX = 45;
+			this.lightObj.rotationY = 110;
+			this.lightObj.rotationZ = 0;
+			let lc = this.lightObj.addComponent(DirectLight);
+			lc.lightColor = KelvinUtil.color_temperature_to_rgb(5355);
+			lc.castShadow = true;
+			lc.intensity = 30;
+			this.scene.addChild(this.lightObj);
+		}
 
-    private sphere: Object3D
+		{
+			let mat = new LitMaterial();
+			mat.baseMap = Engine3D.res.grayTexture;
+			mat.normalMap = Engine3D.res.normalTexture;
+			mat.aoMap = Engine3D.res.whiteTexture;
+			mat.maskMap = Engine3D.res.createTexture(32, 32, 255.0, 255.0, 0.0, 1);
+			mat.emissiveMap = Engine3D.res.blackTexture;
+			mat.roughness = 1.0;
+			mat.metallic = 0.0;
 
-    private createPlane(scene: Scene3D) {
-        let mat = new LitMaterial()
-        mat.baseMap = Engine3D.res.whiteTexture
-        mat.normalMap = Engine3D.res.normalTexture
-        mat.aoMap = Engine3D.res.whiteTexture
-        mat.maskMap = Engine3D.res.createTexture(32, 32, 255.0, 10.0, 0.0, 1)
-        mat.emissiveMap = Engine3D.res.blackTexture
-        mat.roughness = 0.1
-        mat.roughness_max = 0.1
-        mat.metallic = 0.0
+			let floor = new Object3D();
+			let mr = floor.addComponent(MeshRenderer);
+			mr.geometry = new PlaneGeometry(400, 400);
+			mr.material = mat;
+			this.scene.addChild(floor);
 
-        {
-            let debugGeo = new PlaneGeometry(1000, 1000)
-            let obj: Object3D = new Object3D()
-            let mr = obj.addComponent(MeshRenderer)
-            mr.material = mat
-            mr.geometry = debugGeo
-            scene.addChild(obj)
-        }
+			{
+				let wall = new Object3D();
+				let mr = wall.addComponent(MeshRenderer);
+				mr.geometry = new BoxGeometry(5, 260, 320);
+				mr.material = mat;
+				wall.x = -320 * 0.5;
+				this.scene.addChild(wall);
+			}
 
-        {
-            let sphereGeometry = new SphereGeometry(5, 50, 50)
-            let obj: Object3D = new Object3D()
-            let mr = obj.addComponent(MeshRenderer)
-            mr.material = mat
-            mr.geometry = sphereGeometry
-            obj.x = 10
-            obj.y = 2
-            scene.addChild(obj)
-            this.sphere = obj
+			{
+				let wall = new Object3D();
+				let mr = wall.addComponent(MeshRenderer);
+				mr.geometry = new BoxGeometry(5, 260, 320);
+				mr.material = mat;
+				wall.x = 320 * 0.5;
+				this.scene.addChild(wall);
+			}
 
-            const GUIHelp = new dat.GUI()
-            GUIHelp.add(this, 'runBall')
-            GUIHelp.add(this.sphere, 'y', -10, 10, 0.1)
-        }
-    }
+			{
+				let wall = new Object3D();
+				let mr = wall.addComponent(MeshRenderer);
+				mr.geometry = new BoxGeometry(320, 260, 5);
+				mr.material = mat;
+				wall.z = -320 * 0.5;
+				this.scene.addChild(wall);
+			}
 
-    runBall: boolean = true
-    private loop(): void {
-        if (this.sphere && this.runBall) {
-            this.sphere.x = Math.sin(Time.time * 0.001) * 10
-            this.sphere.z = Math.cos(Time.time * 0.001) * 10
-        }
-    }
+			{
+				{
+					let sp = new Object3D();
+					let mr = sp.addComponent(MeshRenderer);
+					mr.geometry = new SphereGeometry(50, 30, 30);
+					mr.material = mat;
+					this.scene.addChild(sp);
+				}
+			}
+		}
+	}
+
+	private gui() {
+		let postProcessing = this.scene.getComponent(PostProcessingComponent);
+		let post = postProcessing.getPost(GTAOPost);
+
+        let GUIHelp = new dat.GUI()
+		let f = GUIHelp.addFolder("GTAO");
+		f.add(post, "maxDistance", 0.0, 50, 1);
+		f.add(post, "maxPixel", 0.0, 50, 1);
+		f.add(post, "rayMarchSegment", 4, 10, 0.001);
+		f.add(post, "darkFactor", 0.0, 5, 0.001);
+		f.add(post, "blendColor");
+		f.add(post, "multiBounce");
+        f.open()
+	}
 }
 
-new Sample_GTAO().run()
+new Sample_GTAO().run();
