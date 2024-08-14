254c530e0a5c7de14bb10c14b15a251f839f0d29
ShuangLiu <liu.shuang@huasheng.io>
2024-08-14 20:15:36 +0800

    update physics demo

---
+++ docs/example/geometry/TextGeometry.md
@@ -0,0 +1,8 @@
+---
+aside: false
+---
+# Terrain
+---
+<Demo src="/examples/geometry/TextGeometry.ts" :code="false" :height="700"></Demo>
+
+<<< @/public/examples/geometry/TextGeometry.ts
--- docs/public/demos/physics/demo2.ts
+++ docs/public/demos/physics/demo2.ts
@@ -1,110 +1,231 @@
-import { BoxColliderShape, BoxGeometry, Camera3D, ColliderComponent, Color, ComponentBase, DirectLight, Engine3D, AtmosphericComponent, View3D, LitMaterial, HoverCameraController, MeshRenderer, Object3D, PlaneGeometry, Scene3D, Vector2, Vector3 } from '@orillusion/core';
-import { Physics, Rigidbody } from '@orillusion/physics';
+import { Engine3D, LitMaterial, MeshRenderer, BoxGeometry, Object3D, Scene3D, View3D, Object3DUtil, Vector3, AtmosphericComponent, DirectLight, SphereGeometry, CameraUtil, HoverCameraController, BitmapTexture2D, VertexAttributeName, Color, CylinderGeometry, TorusGeometry, ComponentBase } from "@orillusion/core";
+import { TerrainGeometry } from "@orillusion/geometry";
+import { Graphic3D } from "@orillusion/graphic";
+import { Ammo, CollisionShapeUtil, Physics, Rigidbody } from "@orillusion/physics";
 
-class Sample_colliders {
-    constructor() {}
-
-    addPlane(scene: Scene3D, size: Vector2, pos: Vector3, rot: Vector3) {
-        const obj = new Object3D();
-        let mr = obj.addComponent(MeshRenderer);
-        mr.geometry = new PlaneGeometry(size.x, size.y);
-        mr.material = new LitMaterial();
-        mr.material.baseColor = new Color(0.04, 0.42, 0.45, 1);
-        obj.localPosition = pos;
-        obj.localRotation = rot;
-        // add a rigidbody with no mass - static body
-        let rigidbody = obj.addComponent(Rigidbody);
-        rigidbody.mass = 0;
-        // add a box collider shape with small y value
-        let collider = obj.addComponent(ColliderComponent);
-        collider.shape = new BoxColliderShape();
-        collider.shape.size = new Vector3(size.x, 0.1, size.y);
-        scene.addChild(obj);
-        return obj;
-    }
-
-    loop() {
-        if (Physics.isInited) {
-            Physics.update();
-        }
-    }
+class Sample_MultipleShapes {
+    scene: Scene3D;
+    terrain: Object3D;
+    gui: dat.GUI;
 
     async run() {
-        // Init physics engine
+        // init physics and engine
         await Physics.init();
-        // Init Engine3D
         await Engine3D.init({
-            canvasConfig: { devicePixelRatio: 1 },
-            renderLoop: () => this.loop()
+            renderLoop: () => Physics.update()
         });
 
-        let scene3D = new Scene3D();
-        scene3D.addComponent(AtmosphericComponent);
-        let cameraObj = new Object3D();
-        let mainCamera = cameraObj.addComponent(Camera3D);
-        mainCamera.lookAt(new Vector3(0, 0, 10), new Vector3(0, 0, 0));
-        mainCamera.perspective(60, Engine3D.aspect, 1, 5000.0);
-        let controller = mainCamera.object3D.addComponent(HoverCameraController);
-        controller.setCamera(45, -15, 150, new Vector3(0, 30, 0));
-        scene3D.addChild(cameraObj);
+        // shadow settings
+        Engine3D.setting.shadow.shadowBias = 0.01;
+        Engine3D.setting.shadow.shadowSize = 1024 * 4;
+        Engine3D.setting.shadow.csmMargin = 0.1;
+        Engine3D.setting.shadow.csmScatteringExp = 0.8;
+        Engine3D.setting.shadow.csmAreaScale = 0.1;
+        Engine3D.setting.shadow.updateFrameRate = 1;
 
-        let light: Object3D = new Object3D();
-        let component = light.addComponent(DirectLight);
-        light.rotationX = 45;
-        light.rotationY = 30;
-        component.lightColor = new Color(1.0, 1.0, 1.0, 1.0);
-        component.intensity = 5;
-        scene3D.addChild(light);
+        this.scene = new Scene3D();
 
-        this.addPlane(scene3D, new Vector2(100, 100), new Vector3(0, 0, 0), new Vector3(0, 0, 0));
-        scene3D.addComponent(BoxGenerator);
+        // Setup camera
+        let camera = CameraUtil.createCamera3DObject(this.scene);
+        camera.perspective(60, Engine3D.aspect, 0.1, 800.0);
+        camera.enableCSM = true;
 
+        let hoverCtrl = camera.object3D.addComponent(HoverCameraController);
+        hoverCtrl.setCamera(0, -25, 100);
+        hoverCtrl.dragSmooth = 4;
+
+        // Create directional light
+        let lightObj3D = new Object3D();
+        lightObj3D.localRotation = new Vector3(-35, -143, 92);
+
+        let light = lightObj3D.addComponent(DirectLight);
+        light.lightColor = Color.COLOR_WHITE;
+        light.castShadow = true;
+        light.intensity = 2.2;
+        this.scene.addChild(light.object3D);
+
+        // init sky
+        let atmosphericSky = this.scene.addComponent(AtmosphericComponent);
+        atmosphericSky.sunY = 0.6;
+
+        // Setup view
         let view = new View3D();
-        view.scene = scene3D;
-        view.camera = mainCamera;
-        // start render
+        view.camera = camera;
+        view.scene = this.scene;
+
         Engine3D.startRenderView(view);
+
+        // init terrain and create static planes
+        await this.initTerrain();
+        this.createStaticPlanes();
+
+        this.scene.addComponent(BoxGenerator);
+    }
+
+    async initTerrain() {
+        // Load textures
+        let bitmapTexture = await Engine3D.res.loadTexture('https://cdn.orillusion.com/terrain/test01/bitmap.png');
+        let heightTexture = await Engine3D.res.loadTexture('https://cdn.orillusion.com/terrain/test01/height.png');
+
+        const width = 100;
+        const height = 100;
+        const terrainMaxHeight = 60;
+        const segment = 60
+
+        // Create terrain geometry
+        let terrainGeometry = new TerrainGeometry(width, height, segment, segment);
+        terrainGeometry.setHeight(heightTexture as BitmapTexture2D, terrainMaxHeight);
+
+        let terrain = new Object3D();
+        let mr = terrain.addComponent(MeshRenderer);
+        mr.geometry = terrainGeometry;
+
+        let mat = new LitMaterial();
+        mat.baseMap = bitmapTexture;
+        mat.metallic = 0;
+        mat.roughness = 1.3;
+        mr.material = mat;
+
+        this.terrain = terrain;
+        this.scene.addChild(terrain);
+
+        // Add rigidbody to terrain
+        let terrainRb = terrain.addComponent(Rigidbody);
+        terrainRb.shape = Rigidbody.collisionShape.createHeightfieldTerrainShape(terrain);
+        terrainRb.mass = 0; // Static rigidbody
+        terrainRb.margin = 0.05;
+        terrainRb.isDisableDebugVisible = true;
+        terrainRb.friction = 1;
+    }
+
+    // Create static planes for boundaries
+    createStaticPlanes() {
+        // Create bottom static plane
+        let staticFloorBottom = Object3DUtil.GetPlane(Engine3D.res.whiteTexture);
+        staticFloorBottom.y = -500;
+        staticFloorBottom.transform.enable = false;
+        this.scene.addChild(staticFloorBottom);
+
+        let bottomRb = staticFloorBottom.addComponent(Rigidbody);
+        bottomRb.shape = CollisionShapeUtil.createStaticPlaneShape();
+        bottomRb.mass = 0;
+
+        // Create top static plane
+        let staticFloorTop = Object3DUtil.GetPlane(Engine3D.res.whiteTexture);
+        staticFloorTop.y = 100;
+        staticFloorTop.transform.enable = false;
+        this.scene.addChild(staticFloorTop);
+
+        let topRb = staticFloorTop.addComponent(Rigidbody);
+        topRb.shape = CollisionShapeUtil.createStaticPlaneShape(Vector3.DOWN);
+        topRb.mass = 0;
     }
 }
 
 class BoxGenerator extends ComponentBase {
-    // save last time
-    private _lastTime: number = performance.now();
+    private lastTime: number = performance.now(); // Save last time
 
-    // a simple loop update
+    public container: Object3D;
+    public interval: number = 1000; // Interval for adding shapes
+    public totalShapes: number = 30; // Maximum number of shapes
+
+    async start() {
+        this.container = new Object3D();
+        this.object3D.addChild(this.container);
+    }
+
+    // Update loop
     public onUpdate(): void {
-        // get current time
         let now: number = performance.now();
-        // add a box every 300ms
-        if (now - this._lastTime > 300) {
-            // add a box
-            this._addBox();
-            // remove the first box after 500 boxes
-            if (this.object3D.entityChildren.length > 500) this.object3D.removeChildByIndex(4);
-            // save current time
-            this._lastTime = now;
+        if (now - this.lastTime > this.interval) {
+            if (this.container.numChildren >= this.totalShapes) {
+                let index = Math.floor(now / this.interval) % this.totalShapes;
+                let shapeObject = this.container.getChildByIndex(index) as Object3D;
+                shapeObject.localPosition.set(Math.random() * 60 - 60 / 2, 40, Math.random() * 60 - 60 / 2);
+                shapeObject.getComponent(Rigidbody).updateTransform(shapeObject.localPosition, null, true);
+            } else {
+                this.addRandomShape();
+            }
+            this.lastTime = now; // Save current time
         }
     }
 
-    // add a box
-    private _addBox(): void {
-        const obj = new Object3D();
-        let mr = obj.addComponent(MeshRenderer);
-        mr.geometry = new BoxGeometry(5, 5, 5);
-        mr.material = new LitMaterial();
-        mr.material.baseColor = new Color(Math.random(), Math.random(), Math.random(), 1.0);
-        obj.localPosition = new Vector3(Math.random() * 70 - 35, 50, Math.random() * 70 - 35);
-        obj.localRotation = new Vector3(Math.random() * 360, Math.random() * 360, Math.random() * 360);
-        // add a rigidbody with mass 10
-        let rigidbody = obj.addComponent(Rigidbody);
-        rigidbody.mass = 10;
-        // add a box collider shape
-        let collider = obj.addComponent(ColliderComponent);
-        collider.shape = new BoxColliderShape();
-        collider.shape.size = new Vector3(5, 5, 5);
+    private addRandomShape(): void {
+        const shapeObject = new Object3D();
+        let mr = shapeObject.addComponent(MeshRenderer);
+        let mat = new LitMaterial();
+        mat.baseColor = Color.random();
 
-        this.object3D.addChild(obj);
+        let size = 1 + Math.random() / 2;
+        let height = 1 + Math.random() * (3 - 1);
+        let radius = 0.5 + Math.random() / 2;
+        const segments = 32;
+
+        let shape: Ammo.btCollisionShape;
+        let shapeType = Math.floor(Math.random() * 6); // Six basic shapes
+        switch (shapeType) {
+            case 0: // Box shape
+                mr.geometry = new BoxGeometry(size, size, size);
+                mr.material = mat;
+                shape = CollisionShapeUtil.createBoxShape(shapeObject);
+                break;
+            case 1: // Sphere shape
+                mr.geometry = new SphereGeometry(radius, segments, segments);
+                mr.material = mat;
+                shape = CollisionShapeUtil.createSphereShape(shapeObject);
+                break;
+            case 2: // Cylinder shape
+                mr.geometry = new CylinderGeometry(radius, radius, height, segments, segments);
+                mr.materials = [mat, mat, mat];
+                shape = CollisionShapeUtil.createCylinderShape(shapeObject);
+                break;
+            case 3: // Cone shape
+                mr.geometry = new CylinderGeometry(0.01, radius, height, segments, segments);
+                mr.materials = [mat, mat, mat];
+                shape = CollisionShapeUtil.createConeShape(shapeObject);
+                break;
+            case 4: // Capsule shape
+                mr.geometry = new CylinderGeometry(radius, radius, height, segments, segments);
+                mr.material = mat;
+                const { r, g, b } = mat.baseColor;
+                let topSphere = Object3DUtil.GetSingleSphere(radius, r, g, b);
+                topSphere.y = height / 2;
+                let bottomSphere = topSphere.clone();
+                bottomSphere.y = -height / 2;
+                shapeObject.addChild(topSphere);
+                shapeObject.addChild(bottomSphere);
+                shape = CollisionShapeUtil.createCapsuleShape(shapeObject);
+                break;
+            case 5: // Torus shape (convex hull shape)
+                mr.geometry = new TorusGeometry(radius, size / 5, segments / 2, segments / 2);
+                mr.material = mat;
+                shape = CollisionShapeUtil.createConvexHullShape(shapeObject);
+                break;
+            default:
+                break;
+        }
+
+        const posRange = 60;
+        shapeObject.x = Math.random() * posRange - posRange / 2;
+        shapeObject.y = 40;
+        shapeObject.z = Math.random() * posRange - posRange / 2;
+
+        shapeObject.localRotation = new Vector3(Math.random() * 360, Math.random() * 360, Math.random() * 360);
+        this.container.addChild(shapeObject);
+
+        // Add rigidbody to shape
+        let rigidbody = shapeObject.addComponent(Rigidbody);
+        rigidbody.shape = shape;
+        rigidbody.mass = Math.random() * 10 + 0.1;
+        rigidbody.rollingFriction = 0.5;
+        rigidbody.damping = [0.1, 0.1];
+
+        // Enable continuous collision detection (CCD)
+        const maxDimension = Math.max(size, height, radius);
+        const ccdMotionThreshold = maxDimension * 0.1; // Set motion threshold to 10% of max dimension
+        const ccdSweptSphereRadius = maxDimension * 0.05; // Set swept sphere radius to 5% of max dimension
+        rigidbody.ccdSettings = [ccdMotionThreshold, ccdSweptSphereRadius];
     }
 }
 
-new Sample_colliders().run();
+new Sample_MultipleShapes().run();
