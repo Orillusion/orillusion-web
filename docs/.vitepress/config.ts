f212c4b7c73efac110b32f9c140cd7a7fe30f801
ShuangLiu <liu.shuang@huasheng.io>
2026-08-04 23:07:28 +0800

    enable local search

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -101,74 +101,74 @@ export default async () =>
                 message: '<a href="https://beian.miit.gov.cn/" target="_blank">京ICP备2021027896号</a>',
                 copyright: 'Copyright © 2021-Present <a href="mailto:business@orillusion.com">Orillusion</a>'
             },
-            // search: {
-            //     provider: 'local',
-            //     options: {
-            //         translations: {
-            //             button: {
-            //                 buttonText: '搜索文档',
-            //                 buttonAriaLabel: '搜索文档'
-            //             },
-            //             modal: {
-            //                 noResultsText: '无法找到相关结果',
-            //                 resetButtonTitle: '清除查询条件',
-            //                 footer: {
-            //                     selectText: '选择',
-            //                     navigateText: '切换',
-            //                     closeText: '关闭'
-            //                 }
-            //             }
-            //         }
-            //     }
-            // },
-
-            algolia: {
-                appId: 'A4SU9IUJ2M',
-                apiKey: '1a3b57b41821e82cafdd80621f042c9c',
-                indexName: 'orillusion',
-                searchParameters: {
-                    facetFilters: ['tags:latest']
-                },
-                placeholder: '搜索文档',
-                translations: {
-                    button: {
-                        buttonText: '搜索文档',
-                        buttonAriaLabel: '搜索文档'
-                    },
-                    modal: {
-                        searchBox: {
-                            resetButtonTitle: '清除查询条件',
-                            resetButtonAriaLabel: '清除查询条件',
-                            cancelButtonText: '取消',
-                            cancelButtonAriaLabel: '取消'
+            search: {
+                provider: 'local',
+                options: {
+                    translations: {
+                        button: {
+                            buttonText: '搜索文档',
+                            buttonAriaLabel: '搜索文档'
                         },
-                        startScreen: {
-                            recentSearchesTitle: '搜索历史',
-                            noRecentSearchesText: '没有搜索历史',
-                            saveRecentSearchButtonTitle: '保存至搜索历史',
-                            removeRecentSearchButtonTitle: '从搜索历史中移除',
-                            favoriteSearchesTitle: '收藏',
-                            removeFavoriteSearchButtonTitle: '从收藏中移除'
-                        },
-                        errorScreen: {
-                            titleText: '无法获取结果',
-                            helpText: '你可能需要检查你的网络连接'
-                        },
-                        footer: {
-                            selectText: '选择',
-                            navigateText: '切换',
-                            closeText: '关闭',
-                            searchByText: '搜索提供者'
-                        },
-                        noResultsScreen: {
+                        modal: {
                             noResultsText: '无法找到相关结果',
-                            suggestedQueryText: '你可以尝试查询',
-                            reportMissingResultsText: '你认为该查询应该有结果？',
-                            reportMissingResultsLinkText: '点击反馈'
+                            resetButtonTitle: '清除查询条件',
+                            footer: {
+                                selectText: '选择',
+                                navigateText: '切换',
+                                closeText: '关闭'
+                            }
                         }
                     }
                 }
             },
+
+            // algolia: {
+            //     appId: 'A4SU9IUJ2M',
+            //     apiKey: '1a3b57b41821e82cafdd80621f042c9c',
+            //     indexName: 'orillusion',
+            //     searchParameters: {
+            //         facetFilters: ['tags:latest']
+            //     },
+            //     placeholder: '搜索文档',
+            //     translations: {
+            //         button: {
+            //             buttonText: '搜索文档',
+            //             buttonAriaLabel: '搜索文档'
+            //         },
+            //         modal: {
+            //             searchBox: {
+            //                 resetButtonTitle: '清除查询条件',
+            //                 resetButtonAriaLabel: '清除查询条件',
+            //                 cancelButtonText: '取消',
+            //                 cancelButtonAriaLabel: '取消'
+            //             },
+            //             startScreen: {
+            //                 recentSearchesTitle: '搜索历史',
+            //                 noRecentSearchesText: '没有搜索历史',
+            //                 saveRecentSearchButtonTitle: '保存至搜索历史',
+            //                 removeRecentSearchButtonTitle: '从搜索历史中移除',
+            //                 favoriteSearchesTitle: '收藏',
+            //                 removeFavoriteSearchButtonTitle: '从收藏中移除'
+            //             },
+            //             errorScreen: {
+            //                 titleText: '无法获取结果',
+            //                 helpText: '你可能需要检查你的网络连接'
+            //             },
+            //             footer: {
+            //                 selectText: '选择',
+            //                 navigateText: '切换',
+            //                 closeText: '关闭',
+            //                 searchByText: '搜索提供者'
+            //             },
+            //             noResultsScreen: {
+            //                 noResultsText: '无法找到相关结果',
+            //                 suggestedQueryText: '你可以尝试查询',
+            //                 reportMissingResultsText: '你认为该查询应该有结果？',
+            //                 reportMissingResultsLinkText: '点击反馈'
+            //             }
+            //         }
+            //     }
+            // },
 
             lastUpdatedText: '最后更新',
 
